c406254a2e897b674851a332e959b208a1f08ba9
Piotr Myszkier <pmyszkier@users.noreply.github.com>
2020-03-01 16:25:08 +0100

    Updates

---
+++ precache-manifest.8f2fed2a1a7a1c4faeda4b97ade9b893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef233d95b31799e4917d90d8053b2d6",
+    "url": "/pmyszkier/react-first-app.git/index.html"
+  },
+  {
+    "revision": "c2608544ccaaf5093e13",
+    "url": "/pmyszkier/react-first-app.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e6a2123ac10cd60f983d",
+    "url": "/pmyszkier/react-first-app.git/static/js/2.6aef294c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pmyszkier/react-first-app.git/static/js/2.6aef294c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2608544ccaaf5093e13",
+    "url": "/pmyszkier/react-first-app.git/static/js/main.e91ee3b5.chunk.js"
+  },
+  {
+    "revision": "96efaff01471e0d8ce5d",
+    "url": "/pmyszkier/react-first-app.git/static/js/runtime-main.8baf4878.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pmyszkier/react-first-app.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
